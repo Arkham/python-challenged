588a803e6e123572bc3063214768e2a8f02d86b7
Ju Liu <ju.liu@welaika.com>
2013-02-21 01:26:06 +0100

    added virtualenv; level 7

---
+++ 7.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from PIL import Image
+from itertools import groupby
+
+im = Image.open("7.png").crop((0,43,608,44))
+colors = list(im.getdata())
+colors = [ elem for index, elem in enumerate(colors) if index % 7 == 0]
+print colors
+print ''.join([ chr(elem[0]) for elem in colors ])
+
+solution = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+print ''.join([ chr(elem) for elem in solution ])
